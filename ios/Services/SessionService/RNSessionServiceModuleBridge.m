17be09b8bceb22ccc2f18405b28d6060d858e623
Yuriy Ganushevich <36405240+yuriymiquido@users.noreply.github.com>
2021-10-19 14:57:15 +0200

    DEXA-199 Migrate existing CommandModule to Swift (#74)

--- ios/Services/SessionService/RNSessionServiceModuleBridge.m
+++ ios/Services/SessionService/RNSessionServiceModuleBridge.m
@@ -3,15 +3,15 @@
 
 @interface RCT_EXTERN_REMAP_MODULE(DolbyIoIAPISessionServiceModule, RNSessionServiceModule, NSObject)
 
-RCT_EXTERN_METHOD(open:(NSDictionary *)userInfo
-				  resolver:(RCTPromiseResolveBlock)resolve
-				  rejecter:(RCTPromiseRejectBlock)reject);
+RCT_EXTERN_METHOD(open:(NSDictionary * _Nonnull)userInfo
+				  resolver:(RCTPromiseResolveBlock _Nonnull)resolve
+				  rejecter:(RCTPromiseRejectBlock _Nonnull)reject);
 
-RCT_EXTERN_METHOD(close:(RCTPromiseResolveBlock)resolve
-				  rejecter:(RCTPromiseRejectBlock)reject);
+RCT_EXTERN_METHOD(close:(RCTPromiseResolveBlock _Nonnull)resolve
+				  rejecter:(RCTPromiseRejectBlock _Nonnull)reject);
 
-RCT_EXTERN_METHOD(getParticipant:(RCTPromiseResolveBlock)resolve
-				  rejecter:(RCTPromiseRejectBlock)reject);
+RCT_EXTERN_METHOD(getParticipant:(RCTPromiseResolveBlock _Nonnull)resolve
+				  rejecter:(RCTPromiseRejectBlock _Nonnull)reject);
 
 @end
 
