170616153e299584690ebec94df5fef77ac33986
Agnieszka Adamczyk <agnieszka.adamczyk@miquido.com>
2021-10-20 10:48:36 +0200

    DEXA-39 Conference Service - fixes after merge with develop

--- android/src/main/java/io/dolby/sdk/reactnative/services/RNConferenceServiceModule.kt
+++ android/src/main/java/io/dolby/sdk/reactnative/services/RNConferenceServiceModule.kt
@@ -2,6 +2,7 @@ package io.dolby.sdk.reactnative.services
 
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.ReadableArray
 import com.facebook.react.bridge.ReadableMap
 import com.voxeet.promise.Promise
 import com.voxeet.sdk.models.Conference
