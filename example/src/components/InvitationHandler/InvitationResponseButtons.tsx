6c69bbe1fdb82dfda8b6e25631b73d867f54af33
arkadiuszkrykM <arkadiusz.kryk@miquido.com>
2021-11-09 11:42:42 +0100

    fixed invitation handler

--- example/src/components/InvitationHandler/InvitationResponseButtons.tsx
+++ example/src/components/InvitationHandler/InvitationResponseButtons.tsx
@@ -9,24 +9,33 @@ import styles from './InvitationResponseButtons.style';
 
 type InvitationEventResponseProps = {
   conferenceId: string;
+  joinWithId: (id: string) => void;
 };
 const InvitationResponseButtons = ({
   conferenceId,
+  joinWithId,
 }: InvitationEventResponseProps) => {
   const decline = async () => {
-    const conference = await DolbyIoIAPI.conference.fetch(conferenceId);
-    await DolbyIoIAPI.notification.decline(conference);
-    RNToast.hide();
+    try {
+      const conference = await DolbyIoIAPI.conference.fetch(conferenceId);
+      await DolbyIoIAPI.notification.decline(conference);
+      RNToast.hide();
+    } catch (e: any) {
+      console.log('couldnt decline conference invitation\n', e.toString());
+    }
   };
   const accept = async () => {
-    const conference = await DolbyIoIAPI.conference.fetch(conferenceId);
-    await DolbyIoIAPI.conference.join(conference);
-    RNToast.hide();
+    try {
+      joinWithId(conferenceId);
+      RNToast.hide();
+    } catch (e: any) {
+      console.log('couldnt accept conference invitation\n', e.toString());
+    }
   };
   return (
     <Space style={styles.container}>
-      <Button color="dark" text="accept" size="small" onPress={decline} />
-      <Button size="small" color="dark" text="decline" onPress={accept} />
+      <Button color="dark" text="accept" size="small" onPress={accept} />
+      <Button size="small" color="dark" text="decline" onPress={decline} />
     </Space>
   );
 };
