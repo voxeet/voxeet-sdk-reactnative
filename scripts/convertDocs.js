601752da50eb924870fa1526b15a7986491a4230
arkadiuszkrykM <arkadiusz.kryk@miquido.com>
2022-01-11 09:39:00 +0100

    --no-verify

--- scripts/convertDocs.js
+++ scripts/convertDocs.js
@@ -1,6 +1,47 @@
 const fs = require('fs');
 const path = require('path');
 
+/** SUMMARY:
+ * This summary should help someone who stumbles upon this file to understand
+ * what and why we're changing.
+ *
+ * It is an internal Dolby requirement for documentation files to have a specific
+ * structure and contents. Quite a lot of things that typedoc library generates
+ * from our typescript files unfortunately doesn't match Dolby requirements. This file
+ * serves as a documentation-compatibility generator that takes originally created
+ * documentation files and convert their contents and their names.
+ * Running the script requires original documentation files to be present in root
+ * `/docs` directory. Newly created files will appear in /scripts/docs directory.
+ *
+ *
+ * KNOWN REQUIREMENTS as of 11.01.2022
+ * 1. Documentation Files need to have a header for SEO and CI/CD purposes - this is
+ * done with createDocHeader() function.
+ *
+ * 2. Documentation Files need to have their slug generated based on their names. This
+ * follows a convention of slugprefix-module-filename e.g.
+ * rn-client-sdk-references-videopresentationservice. This is done with createHeaderSlug(filePath)
+ * function.
+ *
+ * 3. Documentation Files need to have their internal links swapped. Originally we have
+ * links in a format of [`Conference`](../interfaces/internal.Conference.md) - requirement
+ * is to get rid of backticks in [`Conference`] and change link path from ../interfaces/internal.Conference.md
+ * to doc:rn-client-sdk-interfaces-conference - full change should look like:
+ * [Conference](doc:rn-client-sdk-models-conference) (this is already with module
+ * rename from point 4.)
+ *
+ * 4. Documentation Files need to have their module folder names renamed:
+ * interfaces => models
+ * classes => references
+ * This change also includes changes in link paths inside the files and slug names.
+ *
+ * 5. Documentation Files need to have their Table of contents removed. Starting with
+ * the title and following contents beneath (up until major category title e.g.
+ * ## Properties or ## Methods or ## Enumeration members).
+ * One exception is CommsAPI file that should also have its Table of contents removed
+ * but without ## Properties part.
+ */
+
 /**
  * This will match and capture raw documentation filenames (ignoring any prefixes)
  * e.g. it will match `internal.ConferenceService.md` and capture `ConferenceService`
@@ -22,9 +63,14 @@ const REGEXP_MATCH_DOC_MODULE = /docs\/(\w+)\//;
 
 const REGEXP_MATCH_INTERNAL_LINKS = /\[internal].+/g;
 const REGEXP_MATCH_CLASS_TITLES = /# Class: \w+/g;
+const REGEXP_MATCH_DOC_TITLES = /(# Interface:|# Enumeration:|# Namespace:).+/g;
 const REGEXP_MATCH_TABLE_OF_CONTENTS =
-  /(## Table of contents[\S\s*]*)(?=## Methods|## Properties)/g;
+  /(## Table of contents[\S\s*]*)(?=## Properties|## Methods|## Enumeration members)/g;
 
+const REGEXP_MATCH_TABLE_OF_CONTENTS_COMMS_FILE =
+  /(## Table of contents[\S\s*]*)(?=## Properties)/g;
+
+const REGEXP_MATCH_EVENT_HANDLERS = /(### on[\S\s*]*)/g;
 /**
  * This will match and capture all full link descriptions
  * e.g. it will capture `Participant` and Participant from [`Participant`](../interfaces/internal.Participant.md)
@@ -103,13 +149,17 @@ metadata:
 function convertDoc(filePath, index) {
   let renamedFilepath = filePath;
 
+  /** one of requirements is to rename `interfaces` => `models`
+   * and `classes` => `references` do suit dolby documentation naming convention */
   if (/classes/.test(renamedFilepath)) {
     renamedFilepath = renamedFilepath.replace(/classes/, 'references');
   } else if (/interfaces/.test(renamedFilepath)) {
     renamedFilepath = renamedFilepath.replace(/interfaces/, 'models');
   }
+
   let moduleName = getFilePathModuleName(renamedFilepath);
 
+  /** we make sure our file has `internal.MyService` in its name */
   if (isInternalDocFile(renamedFilepath) && moduleName) {
     const rawFileName = filePath.match(REGEXP_MATCH_DOC_FILENAME);
     if (!rawFileName) {
@@ -117,8 +167,10 @@ function convertDoc(filePath, index) {
     }
     const slug = createHeaderSlug(renamedFilepath);
     const header = createDocHeader(rawFileName[1], slug, index);
-    // we use substring to create new path; here we're going from
-    // ../docs/interface/.. to ./docs/interface/..
+
+    /** we use substring to create new path; here we're going from
+    ../docs/interface/.. to ./docs/interface/.. -- we also rename our files to
+     slugnames since it's also part of requirement */
     const newFilePath = path.dirname(renamedFilepath).substring(1);
     const newFileName = newFilePath + `/${slug}.md`;
 
@@ -128,22 +180,35 @@ function convertDoc(filePath, index) {
     }
     const docAsString = fileBuffer.toString();
     const docWithHeader = header + docAsString;
-    const convertedDocAsString = docWithHeader
+    let convertedDocAsString = '';
+    convertedDocAsString = docWithHeader
       .replaceAll(REGEXP_MATCH_DOC_LINKS, (substring) =>
         changeLinkFormatReplacerFn(substring, moduleName)
       )
       .replaceAll(REGEXP_MATCH_INTERNAL_LINKS, '')
       .replaceAll(REGEXP_MATCH_CLASS_TITLES, '')
-      .replaceAll(REGEXP_MATCH_TABLE_OF_CONTENTS, '')
-      .replaceAll(
-        REGEXP_MATCH_BACKTICKS,
-        (substring, captureGroupOne, captureGroupTwo) => {
-          return substring.replace(
-            /(?<=\[)(`(\w+)`)(?=]\(.+\))/,
-            captureGroupTwo
-          );
-        }
+      .replaceAll(REGEXP_MATCH_DOC_TITLES, '')
+      .replaceAll(REGEXP_MATCH_BACKTICKS, (substring, _, captureGroupTwo) => {
+        return substring.replace(
+          /(?<=\[)(`(\w+)`)(?=]\(.+\))/,
+          captureGroupTwo
+        );
+      })
+      .replaceAll(REGEXP_MATCH_EVENT_HANDLERS, (substring, captureGroup) => {
+        return `## Event handlers\n\n${substring}`;
+      });
+    if (rawFileName[1] === 'CommsAPI') {
+      convertedDocAsString = convertedDocAsString.replaceAll(
+        REGEXP_MATCH_TABLE_OF_CONTENTS_COMMS_FILE,
+        ''
       );
+    } else {
+      convertedDocAsString = convertedDocAsString.replaceAll(
+        REGEXP_MATCH_TABLE_OF_CONTENTS,
+        ''
+      );
+    }
+
     try {
       if (!fs.existsSync(newFilePath)) {
         fs.mkdirSync(newFilePath, {
@@ -177,17 +242,17 @@ function buildFilesPathArray(dir) {
 
 /**
  * This function ensures that whatever file we're reading and changing, has `internal`
- * in its path (then it means it's DolbyIoSDK module)
+ * in its path (then it means it's Dolby documentation file)
  */
 function isInternalDocFile(filepath) {
   return !!new RegExp(REGEXP_MATCH_DOC_FILENAME).test(filepath);
 }
 
 /**
- * This function creates header slug (format slug prefix + module + filename in lowercase)
+ * This function creates header slug (format: slug prefix + module + filename in lowercase)
  * e.g. slug for a file `../docs/interfaces/internal.ConferenceMixingOptions.md` should look like
  * `rn-client-sdk-interfaces-conferencemixingoptions`
- * NOTE: at this point we assume filepath is correct (isInternalDocFile function should return true)
+ * NOTE: at this point we assume filepath have internal.MyService filename format
  */
 function createHeaderSlug(filePath) {
   const sdkModule = filePath.match(REGEXP_MATCH_DOC_MODULE)[1];
